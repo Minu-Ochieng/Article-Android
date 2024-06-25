dea67a22b97ef3aeeee38f8da1a801bbebcce8c0
Minu-Ochieng <minuochieng@gmail.com>
2024-06-25 19:27:48 +0300

    added card

--- app/src/main/java/com/akirachix/article/MainActivity.kt
+++ app/src/main/java/com/akirachix/article/MainActivity.kt
@@ -24,14 +24,23 @@ class MainActivity : AppCompatActivity() {
     fun displayAuthors() {
         val article1 = Article("Trevor Noah", "Born A Crime", "Yes", "", "", "12/02/2024", "kadc")
         val article2 = Article("Trevor Noah", "Born A Crime", "Yes", "", "", "12/02/2024", "kadc")
+        val article3 = Article("Trevor Noah", "Born A Crime", "Yes", "", "", "12/02/2024", "kadc")
+        val article4 = Article("Trevor Noah", "Born A Crime", "Yes", "", "", "12/02/2024", "kadc")
+        val article5 = Article("Trevor Noah", "Born A Crime", "Yes", "", "", "12/02/2024", "kadc")
+        val article6 = Article("Trevor Noah", "Born A Crime", "Yes", "", "", "12/02/2024", "kadc")
+        val article7 = Article("Trevor Noah", "Born A Crime", "Yes", "", "", "12/02/2024", "kadc")
+        val article8 = Article("Trevor Noah", "Born A Crime", "Yes", "", "", "12/02/2024", "kadc")
+        val article9 = Article("Trevor Noah", "Born A Crime", "Yes", "", "", "12/02/2024", "kadc")
+        val article10 = Article("Trevor Noah", "Born A Crime", "Yes", "", "", "12/02/2024", "kadc")
 
-        val articleList = listOf(article1, article2)
+
+        val articleList = listOf(article1, article2,article3, article4,article5, article6,article7,article9, article10)
         val articleAdapter = ArticleAdapter(articleList)
 
         binding.rvReader.adapter = articleAdapter
 
 
     }
-    
+
 
 }
